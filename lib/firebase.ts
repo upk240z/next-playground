dcd693023ec39458f8538d6704abc908ab19e9a2
Y.Hasegawa <hasegawa@zaf.jp>
2021-12-31 15:32:45 +0900

    move config to environment

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,9 +1,10 @@
 import {initializeApp} from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFPngZ5qJci8iQp36pFgxKhFcoQhx6btA",
-  projectId: "webstudio-30e6a",
-  appId: "1:711616945712:web:d7b0819f31059e8b9017f5",
-  authDomain: "webstudio-30e6a.firebaseapp.com",
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
 }
+
 export const app = initializeApp(firebaseConfig)
